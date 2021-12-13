c1ba5dbcefa0df5840e54a85276e6eef4a3f7a73
tamamiyasita <46100058+tamamiyasita@users.noreply.github.com>
2021-12-13 16:45:52 +0900

    cmt

---
+++ scene/Level/Home/L_Bar.gd
@@ -0,0 +1,27 @@
+extends RigidBody2D
+
+export var keycode = "ui_left"
+
+# The flipper takes this long to traverse its arc.
+export var snap_time = 0.95
+
+# The flipper's arc is this big, in degrees.
+export var snap_angle = 90
+
+var intermediate_time = 0.0
+
+func _physics_process(delta):
+#	if Input.is_action_just_pressed(keycode):
+#		$AudioStreamPlayer.play()
+	if Input.is_action_pressed(keycode):
+		if intermediate_time < snap_time:
+			intermediate_time += delta
+			if intermediate_time > snap_time:
+				intermediate_time = snap_time
+			set_rotation_degrees((intermediate_time / snap_time) * snap_angle)
+	else:
+		if intermediate_time > 0:
+			intermediate_time -= delta
+			if intermediate_time < 0:
+				intermediate_time = 0
+			set_rotation_degrees((intermediate_time / snap_time) * snap_angle)
