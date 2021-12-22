d9b5864092722fa9dc8fd5bef04c2ea91f3bc562
tamamiyasita <sigorou@gmail.com>
2021-12-22 22:22:42 +0900

    cmt

--- scene/Home/Hero/DefaultPaddle.gd
+++ scene/Home/Hero/DefaultPaddle.gd
@@ -4,3 +4,29 @@ extends Node2D
 onready var paddle_r := $PaddleR
 onready var paddle_l := $PaddleL
 onready var states := $States
+onready var delay_timer := $ChargeTimer
+onready var anime := $AnimationPlayer
+
+export var delay := 3
+var attack_ready := true
+
+
+
+func _ready() -> void:
+	anime.play('attack_ready')
+	
+	
+
+func _on_Paddle_body_entered(body: Node) -> void:
+	if attack_ready:
+		anime.play('RESET')
+		body.states = states
+		body.on_energy = true
+		attack_ready = false
+		delay_timer.start(delay) 
+		
+
+
+func _on_ChargeTimer_timeout() -> void:
+	attack_ready = true
+	anime.play('attack_ready')
